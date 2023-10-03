dcabaea24aca7fc932b29a63f154fa79678b8a6d
LAPTOP-D5ON7DHA\Admin <phanhoangnam234@gmail.com>
2023-10-03 21:46:12 +0700

    ditmemay

---
+++ Commands/NavigateCommand.cs
@@ -0,0 +1,30 @@
+﻿using Community_House_Management.Stores;
+using Community_House_Management.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Community_House_Management.Commands
+{
+    public class NavigateCommand<T> : CommandBase where T : ViewModelBase
+    {
+        private readonly NavigationStore _navigationStore;
+        private Type _objectType;
+        public NavigateCommand(NavigationStore navigationStore, Type objectType)
+        {
+            _navigationStore = navigationStore;
+            if (objectType == null || !typeof(T).IsAssignableFrom(objectType))
+            {
+                throw new ArgumentException("Invalid object type");
+            }
+            _objectType = objectType;
+        }
+
+        public override void Execute(object parameter)
+        {
+
+        }
+    }
+}
