72825e85eacdc302cb2a48997eaf7239255e0de9
LAPTOP-D5ON7DHA\Admin <phanhoangnam234@gmail.com>
2023-10-21 20:43:13 +0700

    ditmemay

--- ViewModels/StartupViewModels/FacilityManagementViewModel.cs
+++ ViewModels/StartupViewModels/FacilityManagementViewModel.cs
@@ -13,6 +13,45 @@ namespace Community_House_Management.ViewModels.StartupViewModels
 {
     public class FacilityManagementViewModel : ViewModelBase
     {
-        public FacilityManagementViewModel(NavigationStore navigationStore) { }
+        private readonly NavigationStore _navigationStore;
+        private bool isLoggedIn;
+        public bool IsLoggedIn
+        {
+            get { return isLoggedIn; }
+            set
+            {
+                if (value != isLoggedIn)
+                {
+                    isLoggedIn = value;
+                    OnPropertyChanged(nameof(IsLoggedIn));
+                }
+            }
+        }
+        private bool isAddFacilityClicked;
+        public bool IsAddFacilityClicked
+        {
+            get { return isAddFacilityClicked; }
+            set
+            {
+                isAddFacilityClicked = value;
+                OnPropertyChanged(nameof(IsAddFacilityClicked));
+            }
+        }
+        public ICommand OpenAddFacilityCommand { get; }
+        public FacilityManagementViewModel(NavigationStore navigationStore, bool isLoggedIn) 
+        { 
+            _navigationStore = navigationStore;
+            this.isLoggedIn = isLoggedIn;
+            OpenAddFacilityCommand = new RelayCommand(ExecuteOpenAddFacilityCommand, CanExecuteOpenAddFacilityCommand);
+        }
+        private void ExecuteOpenAddFacilityCommand(object parameter)
+        {
+            IsAddFacilityClicked = !IsAddFacilityClicked;
+        }
+        private bool CanExecuteOpenAddFacilityCommand(object parameter)
+        {
+            if (isLoggedIn == true) return true;
+            else return false;
+        }
     }
 }
--- ViewModels/StartupViewModels/HouseholdManagementViewModel.cs
+++ ViewModels/StartupViewModels/HouseholdManagementViewModel.cs
@@ -13,6 +13,45 @@ namespace Community_House_Management.ViewModels.StartupViewModels
 {
     public class HouseholdManagementViewModel : ViewModelBase
     {
-        public HouseholdManagementViewModel(NavigationStore navigationStore) { }
+        private readonly NavigationStore _navigationStore;
+        private bool isLoggedIn;
+        public bool IsLoggedIn
+        {
+            get { return isLoggedIn; }
+            set
+            {
+                if (value != isLoggedIn)
+                {
+                    isLoggedIn = value;
+                    OnPropertyChanged(nameof(IsLoggedIn));
+                }
+            }
+        }
+        private bool isAddHouseholdClicked;
+        public bool IsAddHouseholdClicked
+        {
+            get { return isAddHouseholdClicked; }
+            set
+            {
+                isAddHouseholdClicked = value;
+                OnPropertyChanged(nameof(IsAddHouseholdClicked));
+            }
+        }
+        public ICommand OpenAddHouseholdCommand { get; }
+        public HouseholdManagementViewModel(NavigationStore navigationStore, bool IsLoggedIn) 
+        {
+            _navigationStore = navigationStore;
+            this.isLoggedIn = IsLoggedIn;
+            OpenAddHouseholdCommand = new RelayCommand(ExecuteOpenAddHouseholdCommand, CanExecuteOpenAddHouseholdCommand);
+        }
+        private void ExecuteOpenAddHouseholdCommand(object parameter)
+        {
+            IsAddHouseholdClicked = !IsAddHouseholdClicked;
+        }
+        private bool CanExecuteOpenAddHouseholdCommand(object parameter)
+        {
+            if (isLoggedIn == true) return true;
+            else return false;
+        }
     }
 }
