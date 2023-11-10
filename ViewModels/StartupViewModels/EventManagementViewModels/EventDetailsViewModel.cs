ea4cdd0cf605213e010e026fd3ce44f4dd6cb359
LAPTOP-D5ON7DHA\Admin <phanhoangnam234@gmail.com>
2023-11-10 16:41:13 +0700

    eventdetailsview, viewmodel

---
+++ ViewModels/StartupViewModels/EventManagementViewModels/EventDetailsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Community_House_Management.Commands;
+using Community_House_Management.Models;
+using Community_House_Management.Services;
+using Community_House_Management.Stores;
+using Community_House_Management.Views;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Community_House_Management.ViewModels.StartupViewModels.EventManagementViewModels
+{
+    public class EventDetailsViewModel : ViewModelBase
+    {
+        public EventDetailsViewModel(NavigationStore navigationStore, int eventId) 
+        {
+
+        }
+    }
+}
